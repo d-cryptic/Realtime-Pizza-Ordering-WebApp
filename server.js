aa5fc43668f63b53130739bff32e2c6ffded9817
Barun Debnath <barundebnath91@gmail.com>
2022-06-21 12:22:06 +0530

    express server created

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// set template engine
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
