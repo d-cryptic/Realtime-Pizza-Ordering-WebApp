191364d078e2cb1ecb4dba473e4de4c3e1632589
Barun Debnath <barundebnath91@gmail.com>
2022-06-21 14:39:56 +0530

    tailwind installed | laravel mix installed done | bundler setup done

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const expressLayout = require("express-ejs-layouts");
 const path = require("path");
 const PORT = process.env.PORT || 8080;
 
+// Assets
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
