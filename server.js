e85a5a643d2dcb17a5569f1085cae768c9649fed
Barun Debnath <barundebnath91@gmail.com>
2022-06-21 17:07:58 +0530

    cart page done

--- server.js
+++ server.js
@@ -8,14 +8,18 @@ const PORT = process.env.PORT || 8080;
 // Assets
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
 // set template engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
