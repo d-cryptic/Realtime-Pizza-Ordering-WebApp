c8d28058429c5268858f0afdf5b51ce914d899c2
Barun Debnath <barundebnath91@gmail.com>
2022-06-22 12:51:14 +0530

    notification for add to cart done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,29 @@
 import axios from "axios";
+import Noty from "noty";
+
 let addToCart = document.querySelectorAll(".add-to-cart");
 let cartCounter = document.querySelector("#cartCounter");
 
 function updateCart(pizza) {
-  axios.post("/update-cart", pizza).then((res) => {
-    console.log(res);
-    cartCounter.innerText = res.data.totalQty;
-  });
+  axios
+    .post("/update-cart", pizza)
+    .then((res) => {
+      cartCounter.innerText = res.data.totalQty;
+      new Noty({
+        type: "success",
+        timeout: 1000,
+        text: "Item added to cart",
+        progressBar: false,
+      }).show();
+    })
+    .catch((err) => {
+      new Noty({
+        type: "error",
+        timeout: 1000,
+        text: "Something went wrong",
+        progressBar: false,
+      });
+    });
 }
 
 addToCart.forEach((btn) => {
